b4eb2c818266626775b527c98a338d7f9a5dd448
jiashilin <jsl@pku.edu.cn>
2015-10-06 20:31:15 +0800

    提交

---
+++ FirstHomeWork/119.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> relist=new ArrayList<Integer>(rowIndex);
+		  if(rowIndex<=0)
+	        	return relist;
+		  
+		  for(int i=0;i<=rowIndex;i++){
+			  relist.add(1);
+	        	for(int j=i-1;j>0;j--){//从后往前进行遍历
+	        		/* relist.add(1);
+	        		if(j==0||j==i){
+	        			//relist.add(1);
+	        			relist.set(j, 1);
+	        		}else{
+	        			//relist.add(relist.get(j-1)+relist.get(j));
+	        			relist.set(j, relist.get(j-1)+relist.get(j));
+	        		}*/
+	        		
+	        		relist.set(j, relist.get(j-1)+relist.get(j));
+	        	}
+	        	
+	        }
+		return relist;
+    }
+}
